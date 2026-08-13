2f8663bfccd4f8389bde3db2977a0848fa3a0685
adm400ba <querojogaminecraft184@gmail.com>
2026-08-13 17:40:54 -0300

    Update Example.lua

--- Example.lua
+++ Example.lua
@@ -1,7 +1,5 @@
-local Env = getgenv()
-
-Env.api_key = "gsk_123" -- Groq Api Key
-Env.tts_endpoint = "https://your-app.up.railway.app/gerar-audio" -- https://github.com/adm400ba/edgetts
-Env.yt_dlp_endpoint = "https://your-app.up.railway.app" -- https://github.com/adm400ba/yt-dlp-endpoint
+getgenv().api_key = "gsk_123" -- Groq Api Key
+getgenv().tts_endpoint = "https://your-app.up.railway.app/gerar-audio" -- https://github.com/adm400ba/edgetts
+getgenv().yt_dlp_endpoint = "https://your-app.up.railway.app" -- https://github.com/adm400ba/yt-dlp-endpoint
 
 loadstring(game:HttpGet("https://github.com/adm400ba/Jarvis/raw/refs/heads/main/src/Jarvis.lua"))()
